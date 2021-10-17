78c122523794591271edcbd04491b2e917cc186a
John Lennox <jl1998@hotmail.co.uk>
2021-10-17 16:46:25 +0100

    Issue #3 Create Service
    Created the ContactService interface and added the methods that the old project used.
    I created the ContactServiceImpl class and had it implement ContactService interface
    I allowed Intellij to Auto-create methods with dummy implementation (return null) for each of the methods in ContactServiceImpl
    Created a test class for ContactServiceImpl

---
+++ src/test/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/service/ContactServiceImplTests.java
@@ -0,0 +1,12 @@
+package org.wicket.ms.calltree.msbcpcalltreecontactservice.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactServiceImplTests {
+    @Test
+    void contextLoads(){
+
+    }
+}
