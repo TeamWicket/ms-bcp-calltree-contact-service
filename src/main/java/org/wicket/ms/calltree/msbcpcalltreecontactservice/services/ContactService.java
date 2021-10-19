40be28582f47bbb9b32bfc8dbcf1342e8a79f263
John Lennox <jl1998@hotmail.co.uk>
2021-10-19 22:29:15 +0100

    Issue #3 Create Service
    Created the ContactService interface
    I created the ContactServiceImpl class
    Created a test class for ContactServiceImpl

--- src/main/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/services/ContactService.java
+++ src/main/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/services/ContactService.java
@@ -1,30 +1,4 @@
 package org.wicket.ms.calltree.msbcpcalltreecontactservice.services;
 
-import org.springframework.lang.Nullable;
-import org.wicket.ms.calltree.msbcpcalltreecontactservice.dto.ContactDto;
-import org.wicket.ms.calltree.msbcpcalltreecontactservice.enums.Role;
-
-import java.util.List;
-
 public interface ContactService {
-    List<ContactDto> saveList(List<ContactDto> dtoList);
-
-    ContactDto saveOrUpdate(ContactDto contactDto);
-
-    void deleteContact(Long id);
-
-    Integer getNumContacts();
-
-    List<ContactDto> getAllContacts(@Nullable String orderDirection, @Nullable String orderByValue,
-                                    @Nullable Integer page, @Nullable Integer size);
-
-    ContactDto getContact(Long id);
-
-    List<ContactDto> getAllSelectedRole(Role role);
-
-    List<ContactDto> getCalltreeUntilRole(Role role);
-
-    ContactDto fetchContactByPhoneNumber(String string);
-
-    List<ContactDto> fetchManyContactsById(long[] id);
 }
--- src/main/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/services/ContactServiceImpl.java
+++ src/main/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/services/ContactServiceImpl.java
@@ -1,59 +1,5 @@
 package org.wicket.ms.calltree.msbcpcalltreecontactservice.services;
 
-import org.springframework.lang.Nullable;
-import org.wicket.ms.calltree.msbcpcalltreecontactservice.dto.ContactDto;
-import org.wicket.ms.calltree.msbcpcalltreecontactservice.enums.Role;
-
-import java.util.List;
-
 public class ContactServiceImpl implements ContactService{
-    @Override
-    public List<ContactDto> saveList(List<ContactDto> dtoList) {
-        return null;
-    }
 
-    @Override
-    public ContactDto saveOrUpdate(ContactDto contactDto) {
-        return null;
-    }
-
-    @Override
-    public void deleteContact(Long id) {
-
-    }
-
-    @Override
-    public Integer getNumContacts() {
-        return null;
-    }
-
-    @Override
-    public List<ContactDto> getAllContacts(@Nullable String orderDirection, @Nullable String orderByValue, @Nullable Integer page, @Nullable Integer size) {
-        return null;
-    }
-
-    @Override
-    public ContactDto getContact(Long id) {
-        return null;
-    }
-
-    @Override
-    public List<ContactDto> getAllSelectedRole(Role role) {
-        return null;
-    }
-
-    @Override
-    public List<ContactDto> getCalltreeUntilRole(Role role) {
-        return null;
-    }
-
-    @Override
-    public ContactDto fetchContactByPhoneNumber(String string) {
-        return null;
-    }
-
-    @Override
-    public List<ContactDto> fetchManyContactsById(long[] id) {
-        return null;
-    }
 }
