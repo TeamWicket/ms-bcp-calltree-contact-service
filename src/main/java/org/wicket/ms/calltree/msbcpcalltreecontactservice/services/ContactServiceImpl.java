e2d20921fcb567f8444d4290bba966b9c718c762
John Lennox <jl1998@hotmail.co.uk>
2021-11-04 22:18:06 +0000

    Issue #14 saveOrUpdate
    Added the code to save or update a contact in the contact repository
    Added unit tests

--- src/main/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/services/ContactServiceImpl.java
+++ src/main/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/services/ContactServiceImpl.java
@@ -1,5 +1,23 @@
 package org.wicket.ms.calltree.msbcpcalltreecontactservice.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.wicket.ms.calltree.msbcpcalltreecontactservice.dto.ContactDto;
+import org.wicket.ms.calltree.msbcpcalltreecontactservice.mappers.ContactMapper;
+import org.wicket.ms.calltree.msbcpcalltreecontactservice.models.Contact;
+import org.wicket.ms.calltree.msbcpcalltreecontactservice.repository.ContactRepository;
+
+@Service
+@RequiredArgsConstructor
 public class ContactServiceImpl implements ContactService{
 
+    private final ContactRepository repository;
+    private final ContactMapper mapper;
+
+    @Override
+    public ContactDto saveOrUpdate(ContactDto contactDto) {
+        Contact newContact = mapper.dtoToContact(contactDto);
+        Contact savedContact = repository.save(newContact);
+        return mapper.contactToDto(savedContact);
+    }
 }
