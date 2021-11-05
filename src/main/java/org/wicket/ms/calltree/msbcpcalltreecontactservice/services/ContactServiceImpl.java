f8232da5d3d79b9b91bc781c90ae6b8ff40650df
John Lennox <jl1998@hotmail.co.uk>
2021-11-05 21:45:32 +0000

    Issue #15 ContactService - saveList
    Added the code to save a list of contacts
    
    wrote unit tests for passing a list of multiple sizes and a null

--- src/main/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/services/ContactServiceImpl.java
+++ src/main/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/services/ContactServiceImpl.java
@@ -7,6 +7,8 @@ import org.wicket.ms.calltree.msbcpcalltreecontactservice.mappers.ContactMapper;
 import org.wicket.ms.calltree.msbcpcalltreecontactservice.models.Contact;
 import org.wicket.ms.calltree.msbcpcalltreecontactservice.repository.ContactRepository;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ContactServiceImpl implements ContactService{
@@ -19,5 +21,21 @@ public class ContactServiceImpl implements ContactService{
         Contact newContact = mapper.dtoToContact(contactDto);
         Contact savedContact = repository.save(newContact);
         return mapper.contactToDto(savedContact);
+    }
+
+    @Override
+    public List<ContactDto> saveList(List<ContactDto> contactDtoList) {
+
+        List<Contact> newContactList = contactDtoList
+                .stream()
+                .map(mapper::dtoToContact)
+                .toList();
+
+        List<Contact> savedContactList = repository.saveAll(newContactList);
+
+        return savedContactList
+                .stream()
+                .map(mapper::contactToDto)
+                .toList();
     }
 }
--- src/test/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/services/ContactServiceImplTests.java
+++ src/test/java/org/wicket/ms/calltree/msbcpcalltreecontactservice/services/ContactServiceImplTests.java
@@ -2,8 +2,7 @@ package org.wicket.ms.calltree.msbcpcalltreecontactservice.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.wicket.ms.calltree.msbcpcalltreecontactservice.dto.ContactDto;
 import org.wicket.ms.calltree.msbcpcalltreecontactservice.enums.Role;
@@ -11,7 +10,11 @@ import org.wicket.ms.calltree.msbcpcalltreecontactservice.mappers.ContactMapper;
 import org.wicket.ms.calltree.msbcpcalltreecontactservice.models.Contact;
 import org.wicket.ms.calltree.msbcpcalltreecontactservice.repository.ContactRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -24,6 +27,15 @@ class ContactServiceImplTests {
 
     @Mock
     ContactMapper contactMapper;
+
+    @Captor
+    ArgumentCaptor<List<Contact>> contactListArgumentCaptor;
+
+    @Captor
+    ArgumentCaptor<Contact> contactArgumentCaptor;
+
+    @Captor
+    ArgumentCaptor<ContactDto> contactDtoArgumentCaptor;
 
     @Test
     void saveOrUpdate_throwsError_givenNull() {
@@ -91,5 +103,97 @@ class ContactServiceImplTests {
         when(contactRepository.save(contact)).thenReturn(contact);
 
         assertEquals(contactDto, contactService.saveOrUpdate(contactDto));
+    }
+
+    @Test
+    void saveList_throwsError_givenNull() {
+        when(contactRepository.saveAll(null)).thenThrow(IllegalArgumentException.class);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            contactService.saveList(null);
+        });
+    }
+
+    @Test
+    void saveList_returnsListContactDto_givenListOfContactDtos(){
+        Contact contact = Contact.builder()
+                .id(1L)
+                .firstName("John")
+                .lastName("Doe")
+                .role(Role.REPORTER)
+                .build();
+
+        ContactDto contactDto = ContactDto.builder()
+                .id(1L)
+                .firstName("John")
+                .lastName("Doe")
+                .role(Role.REPORTER)
+                .build();
+
+        List<ContactDto> contactDtoList = new ArrayList<>();
+        List<Contact> contactList = new ArrayList<>();
+        contactDtoList.add(contactDto);
+        contactList.add(contact);
+
+
+        when(contactMapper.dtoToContact(contactDto)).thenReturn(contact);
+        when(contactMapper.contactToDto(contact)).thenReturn(contactDto);
+        when(contactRepository.saveAll(contactList)).thenReturn(contactList);
+
+        assertEquals(contactDtoList, contactService.saveList(contactDtoList));
+        Mockito.verify(contactRepository, Mockito.times(1)).saveAll(contactListArgumentCaptor.capture());
+        Mockito.verify(contactMapper, Mockito.times(1)).contactToDto(contactArgumentCaptor.capture());
+        Mockito.verify(contactMapper, Mockito.times(1)).dtoToContact(contactDtoArgumentCaptor.capture());
+    }
+
+    @Test
+    void saveList_returnsListContactDtos_givenMultipleContactDtos(){
+        Contact contact = Contact.builder()
+                .id(1L)
+                .firstName("John")
+                .lastName("Doe")
+                .role(Role.REPORTER)
+                .build();
+
+        Contact contact2 = Contact.builder()
+                .id(2L)
+                .firstName("John")
+                .lastName("Doe")
+                .role(Role.REPORTER)
+                .build();
+
+        ContactDto contactDto = ContactDto.builder()
+                .id(1L)
+                .firstName("John")
+                .lastName("Doe")
+                .role(Role.REPORTER)
+                .build();
+
+        ContactDto contact2Dto = ContactDto.builder()
+                .id(2L)
+                .firstName("John")
+                .lastName("Doe")
+                .role(Role.REPORTER)
+                .build();
+
+        List<ContactDto> contactDtoList = new ArrayList<>();
+        List<Contact> contactList = new ArrayList<>();
+
+        contactDtoList.add(contactDto);
+        contactDtoList.add(contact2Dto);
+
+        contactList.add(contact);
+        contactList.add(contact2);
+
+        when(contactMapper.dtoToContact(contactDto)).thenReturn(contact);
+        when(contactMapper.dtoToContact(contact2Dto)).thenReturn(contact2);
+        when(contactMapper.contactToDto(contact)).thenReturn(contactDto);
+        when(contactMapper.contactToDto(contact2)).thenReturn(contact2Dto);
+
+        when(contactRepository.saveAll(contactList)).thenReturn(contactList);
+
+        assertEquals(contactDtoList, contactService.saveList(contactDtoList));
+        Mockito.verify(contactRepository, Mockito.times(1)).saveAll(contactListArgumentCaptor.capture());
+        Mockito.verify(contactMapper, Mockito.times(2)).contactToDto(contactArgumentCaptor.capture());
+        Mockito.verify(contactMapper, Mockito.times(2)).dtoToContact(contactDtoArgumentCaptor.capture());
     }
 }
